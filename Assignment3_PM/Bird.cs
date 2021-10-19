85fad546120deff3d3cec7315fb9875d6a05e964
prince <A00229731@mycambrian.ca>
2021-10-18 23:25:32 -0400

    Assignment 3 work

---
+++ Assignment3_PM/Bird.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Assignment3_PM.Eggs;
+namespace Assignment3_PM.Birds
+{
+	class Bird
+	{
+		public static Random Rand = new Random(1);
+		public virtual Egg[] LayEggs(int numEggs)
+		{
+			Console.Error.WriteLine("Bird.LayEggs should not be called!");
+			return new Egg[0];
+		}
+	}
+
+	class Chicken : Bird
+	{
+		public override Egg[] LayEggs(int numEggs)
+		{
+		   Egg[] chicken = new Egg[numEggs];
+		   for(int i=0; i<numEggs; ++i )
+			{
+			    var chickenegg = new Egg(Rand.Next(2, 4), Egg.Colors.brown);
+				chicken[i] = chickenegg;
+
+			}
+	        return chicken;
+		}
+     }
+
+	class Ostrich : Bird
+	{
+		public override Egg[] LayEggs(int numEggs)
+		{
+			Egg[] ostrich = new Egg[numEggs];
+			for (int i = 0; i < numEggs; ++i)
+			{
+				var ostrichegg = new Egg(Rand.Next(10, 14), Egg.Colors.speckled);
+				ostrich[i] = ostrichegg;
+
+			}
+			return ostrich;
+		}
+	}
+}
---
+++ Assignment3_PM/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Assignment3_PM.Birds;
+using Assignment3_PM.Eggs;
+
+namespace Assignment3_PM
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Bird bird;
+				Console.Write("Press C for chicken or O for Ostrich: ");
+				char key = Char.ToUpper(Console.ReadKey().KeyChar);
+				switch (key)
+				{
+					case 'C':
+						bird = new Chicken();
+						break;
+					case 'O':
+						bird = new Ostrich();
+						break;
+					default:
+						return;
+				}
+				Console.Write("\nHow many eggs should it lay? ");
+				if (!int.TryParse(Console.ReadLine(), out int numEggs)) { return; }
+				Egg[] eggs = bird.LayEggs(numEggs);
+				DisplayEggs(eggs);
+			}
+		}
+
+		static void DisplayEggs(Egg[] eggs)
+		{
+			foreach (var egg in eggs)
+			{
+				Console.WriteLine(egg);
+			}
+		}
+	}
+}
